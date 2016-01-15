4b6c2ff3784e9e0b85f66085b1cb70811ac34b63
koheku <phkoeck@gmail.com>
2016-01-15 16:27:24 +0100

    Add todo actions

--- swift-flow-todomvc/swift-flow/Actions/TodoActions.swift
+++ swift-flow-todomvc/swift-flow/Actions/TodoActions.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2016 Paul-Henri Koeck. All rights reserved.
 //
 
-import Foundation
+import SwiftFlow
+
+struct AddTodo: Action {
+    
+    static let type = "ADD_TODO"
+    let text: String
+}
+
+struct DeleteTodo: Action {
+    
+    static let type = "DELETE_TODO"
+    let id: Int
+}
+
+struct EditTodo: Action {
+    
+    static let type = "EDIT_TODO"
+    let id: String
+    let text: String
+}
+
+struct CompleteTodo: Action {
+    
+    static let type = "COMPLETE_TODO"
+    let id: String
+}
+
+struct CompleteAll: Action {
+    
+    static let type = "COMPLETE_ALL"
+}
+
+struct ClearCompleted: Action {
+    
+    static let type = "CLEAR_COMPLETED"
+}
---
+++ swift-flow-todomvc/swift-flow/Store/AppState.swift
@@ -0,0 +1,15 @@
+//
+//  AppState.swift
+//  swift-flow-todomvc
+//
+//  Created by Paul-Henri Koeck on 15/01/16.
+//  Copyright © 2016 Paul-Henri Koeck. All rights reserved.
+//
+
+import SwiftFlow
+
+struct Todo {
+    let text: String
+    let completed: Bool
+    let id: Int
+}
