d333272339b6b0f94f5c8d4ad5aa73a7e02d2dd1
koheku <phkoeck@gmail.com>
2016-01-15 12:01:30 +0100

    Initial Commit

---
+++ swift-flow-todomvcTests/swift_flow_todomvcTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_flow_todomvcTests.swift
+//  swift-flow-todomvcTests
+//
+//  Created by Paul-Henri Koeck on 15/01/16.
+//  Copyright © 2016 Paul-Henri Koeck. All rights reserved.
+//
+
+import XCTest
+@testable import swift_flow_todomvc
+
+class swift_flow_todomvcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
